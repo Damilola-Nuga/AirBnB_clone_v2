faeb93de5a195bdb24a858733faf531347ec2f0b
Damilola-Nuga <58979551+Damilola-Nuga@users.noreply.github.com>
2024-04-21 16:07:59 +0100

    Tasks

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,6 @@
--- Create script that prepares a MySQL server for the project
+-- Prepares the MySQL server for this project
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS hbnb_test@localhost;
-SET PASSWORD FOR hbnb_test@localhost = 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO hbnb_test@localhost;
-GRANT SELECT ON performance_schema.* TO hbnb_test@localhost;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;

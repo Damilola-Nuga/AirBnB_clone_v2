11ac5299fdebd83f02d3b43c25024e38ee257edd
Damilola-Nuga <damiloladaniel357@gmail.com>
2024-05-06 18:31:36 -0700

    tasks

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['54.89.109.87', '100.25.190.21']
+env.hosts = ['54.167.148.198', '34.201.61.181']
 
 
 def do_deploy(archive_path):
